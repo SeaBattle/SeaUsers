30618cb5afe797f2183d5d834c5052d228728654
Tihon <com.tihon@mail.ru>
2016-07-10 14:45:36 +0300

    made node registration stub

--- src/main/seausers_app.erl
+++ src/main/seausers_app.erl
@@ -2,6 +2,8 @@
 
 -behaviour(application).
 
+-include_lib("seaconfig/include/sc_headers.hrl").
+
 %% Application callbacks
 -export([start/2, stop/1]).
 
@@ -13,7 +15,13 @@ start(_StartType, _StartArgs) ->
   Ret = su_super_sup:start_link(),
   {ok, _} = su_database_man:init(),
   {ok, _} = su_cache_man:init(),
+  true = register_node(),
   Ret.
 
 stop(_State) ->
   ok.
+
+
+%% @private
+register_node() ->
+  sc_conf_holder:set_inorder_conf(?USER_SERVICE_HOSTS, <<"127.0.0.1">>).  %TODO - invent the way to register node by public ip. May be use os var?
